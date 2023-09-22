b8e4025905613b5683602ab234c40214918e3c57
joshtayl467 <joshtayl467@SDS-L-19QLG63>
2023-09-22 10:40:02 -0400

    Added Sound

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -30,48 +30,73 @@
         {
             this.countdownlabel = new System.Windows.Forms.Label();
             this.startbutton = new System.Windows.Forms.Button();
+            this.secretbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // countdownlabel
             // 
-            this.countdownlabel.AutoSize = true;
+            this.countdownlabel.BackColor = System.Drawing.Color.Transparent;
             this.countdownlabel.Cursor = System.Windows.Forms.Cursors.Default;
-            this.countdownlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.countdownlabel.Location = new System.Drawing.Point(316, 135);
+            this.countdownlabel.Font = new System.Drawing.Font("Poor Richard", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownlabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.countdownlabel.Location = new System.Drawing.Point(42, 91);
             this.countdownlabel.Name = "countdownlabel";
-            this.countdownlabel.Size = new System.Drawing.Size(122, 52);
+            this.countdownlabel.Size = new System.Drawing.Size(677, 244);
             this.countdownlabel.TabIndex = 1;
-            this.countdownlabel.Text = "Hello";
+            this.countdownlabel.Text = " ";
+            this.countdownlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.countdownlabel.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // startbutton
             // 
-            this.startbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.startbutton.Location = new System.Drawing.Point(315, 303);
+            this.startbutton.BackColor = System.Drawing.Color.White;
+            this.startbutton.Font = new System.Drawing.Font("Poor Richard", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startbutton.ForeColor = System.Drawing.Color.Black;
+            this.startbutton.Location = new System.Drawing.Point(315, 180);
             this.startbutton.Name = "startbutton";
             this.startbutton.Size = new System.Drawing.Size(123, 57);
             this.startbutton.TabIndex = 2;
             this.startbutton.Text = "Start";
-            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.UseVisualStyleBackColor = false;
             this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // secretbutton
+            // 
+            this.secretbutton.BackColor = System.Drawing.Color.Black;
+            this.secretbutton.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.secretbutton.FlatAppearance.BorderSize = 0;
+            this.secretbutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.secretbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.secretbutton.ForeColor = System.Drawing.Color.Transparent;
+            this.secretbutton.Location = new System.Drawing.Point(595, 91);
+            this.secretbutton.Name = "secretbutton";
+            this.secretbutton.Size = new System.Drawing.Size(48, 44);
+            this.secretbutton.TabIndex = 3;
+            this.secretbutton.Text = " ";
+            this.secretbutton.UseVisualStyleBackColor = false;
+            this.secretbutton.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.secretbutton);
             this.Controls.Add(this.startbutton);
             this.Controls.Add(this.countdownlabel);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Label countdownlabel;
         private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Button secretbutton;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -5,8 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.Media;
 
 namespace WindowsFormsApp2
 {
@@ -29,15 +30,85 @@ namespace WindowsFormsApp2
 
         private void startbutton_Click(object sender, EventArgs e)
         {
+            SoundPlayer hornPlayer = new SoundPlayer(Properties.Resources.Horn);
+            // Hide the button when it's clicked
+            startbutton.Visible = false;
+            secretbutton.Visible = false;
             countdownlabel.Text = "Game Starting";
             Refresh();
-            Thread.Sleep(3000);
+            Thread.Sleep(300);
+            countdownlabel.Text = "Game Starting.";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "Game Starting..";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "Game Starting...";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "Game Starting";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "Game Starting.";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "Game Starting..";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "Game Starting...";
+            Refresh();
+            Thread.Sleep(300);
             countdownlabel.Text = "3";
-                Thread.Sleep(3000);
+            Refresh();
+            Thread.Sleep(1000);
             countdownlabel.Text = "2";
+            Refresh();
+            Thread.Sleep(1000);
+            countdownlabel.Text = "1";
+            Refresh();
+            hornPlayer.Play();
+            Thread.Sleep(800);
+            this.BackColor = Color.Green;
+            countdownlabel.Text = "Go!";
 
 
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Hide the button when it's clicked
+            secretbutton.Visible = false;
+            startbutton.Visible = false;
+            countdownlabel.Text = "You Found The Secret Button!";
+            Refresh();
+            Thread.Sleep(1500);
+            countdownlabel.Text = "You get.";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "You get..";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "You get...";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "You get.";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "You get..";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "You get...";
+            Refresh();
+            Thread.Sleep(300);
+            countdownlabel.Text = "A  Cookie";
+            Refresh();
+            Thread.Sleep(700);
         }
     }
 }
